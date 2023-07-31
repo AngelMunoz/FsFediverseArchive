d173abc514241711910ecd6d69afb10cf38c3bef
Angel D. Munoz <scythes.lol@gmail.com>
2023-07-31 13:56:40 -0600

    remove  elmish parser use SRTP to extract

--- src/MkLib/Clients/Router.fs
+++ src/MkLib/Clients/Router.fs
@@ -1,27 +1,53 @@
 namespace MkLib.Clients.Router
 
 open MkLib.Clients.Types
+open FsToolkit.ErrorHandling
 
-type Routes =
-  | Notes
+[<Struct>]
+type Route =
+  | Notes of pagination: Pagination
   | Note of noteId: string
 
-  static member FromParams page limit =
-    match page, limit with
-    | Some page, Some limit -> { page = page; limit = limit }
-    | None, Some limit -> { page = 1; limit = limit }
-    | Some page, None -> { page = page; limit = 10 }
-    | None, None -> { page = 1; limit = 10 }
+  static member FromParams page limit noteId =
+    match noteId with
+    | Some value -> Note value
+    | None ->
+      let pagination =
+        match page, limit with
+        | Some page, Some limit -> { page = page; limit = limit }
+        | None, Some limit -> { page = 1; limit = limit }
+        | Some page, None -> { page = page; limit = 10 }
+        | None, None -> { page = 1; limit = 10 }
+
+      Notes pagination
 
 [<RequireQualifiedAccess>]
 module Router =
-  open Elmish.UrlParser
 
-  let get route =
-    parseUrl (oneOf [ map Note (s "notes" </> str); map Notes (s "notes" </> top) ]) route
+  let inline private getKey
+    (container: ^Container when ^Container: (member TryGetValue: string * byref<obj> -> bool))
+    key
+    =
+    let mutable output = Unchecked.defaultof<obj>
 
-  let query route =
-    parseUrl (map (Routes.FromParams) (top <?> intParam "page" <?> intParam "limit")) route
-    |> function
-      | Some pagination -> pagination
-      | None -> { page = 1; limit = 10 }
+    let parsed = container.TryGetValue(key, &output)
+
+    match parsed with
+    | true -> ValueSome(output.ToString())
+    | _ -> ValueNone
+
+
+  let inline get query =
+    let page =
+      getKey query "page"
+      |> ValueOption.bind ValueOption.tryParse
+      |> ValueOption.defaultValue 1
+
+    let limit =
+      getKey query "limit"
+      |> ValueOption.bind ValueOption.tryParse
+      |> ValueOption.defaultValue 10
+
+    match getKey query "note" with
+    | ValueSome value -> Note value
+    | ValueNone -> Notes { page = page; limit = limit }
